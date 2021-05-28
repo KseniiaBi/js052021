cbab1622d2471e79b9972785499b3c337d369127
Bohdan <borysenkobohdan@gmail.com>
2021-05-29 01:30:03 +0300

    twoTasksDone
    /*--TACK#1--Если переменная test равна true, то выведите на экран слово 'верно', иначе выведите слово 'неверно'.--*/
    /*--TACK#2---Найдите количество секунд в сутках--*/

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert(111);
+/*--TACK#1--Если переменная test равна true, то выведите на экран слово 'верно', иначе выведите слово 'неверно'.--*/
+
+const task_two = prompt("Add for Number")
+let test = true;
+let i = false;
+
+if (task_two >= i){
+    console.log(alert("true"))
+} else {
+    console.log(test <= task_two + alert("false"));
+}
+
+
+/*--TACK#2---Найдите количество секунд в сутках--*/
+
+let min = 60;
+let sec = 60;
+let hour = 24;
+const result = min * sec * hour; 
+console.log(result);
