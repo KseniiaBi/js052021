4af40a585f60f4b3b4a656f9c648274059ffebf7
ant-dasha <85036996+ant-dasha@users.noreply.github.com>
2021-06-04 00:56:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
-let month = 12;
-if (month = 1, 2, 12) {
-    console.log ('Зима');
-}
-else if (month >= 3) {
-    console.log ('Весна');
-}
-else if (month < 6) {
-    console.log('Весна')
-}
-else if (month >= 6) {
-    console.log ('Лето');
-}
-else if (month < 9) {
-    console.log ('Лето');
-}
-else if (month >= 9) {
-    console.log ('Осень');
-}
-else if (month < 12) {
-    console.log ('Зима');
+//Задание 1
+for (let num = 0; num < 100; num++) {
+    if (num % 2 == 0) {
+        console.log(num);
+    }
 }
 
+//Задание 2
+let arr = [1, 2, 3, 4, 5];
+let result = 0;
+for (let i = 0; i < arr.length; i++) {
+    result += arr [i];
+}
+console.log(result);
 
-/*Если переменная test равна true, то выведите на экран слово 'верно', иначе выведите слово 'неверно'.*/
- let test = true;
- if (test = true) {
-     alert ('Верно');
- }
- else {
-     alert ('Неверно')
- }
-  
- /*Найдите количество секунд в сутках.*/
-let d = 24;
-let h = 60;
-let m = 60;
- alert (d*h*m)
+// Задание 3
+let array = [2, 5, 9, 15, 0, 4];
+for (let i = 0; i < array.length; i++) {
+    if (array [i] > 3 && array [i] < 10)
+    console.log(array [i]);
+} 
+ 
+//Задание 4
+let arra = [5, 8, 10, -12, -100, 90];
+let res = 0;
+for (let i = 0; i < arra.length; i++) {
+    if  (arra[i] >= 0) {
+        res += arra[i]
+    }
+} console.log(res);
 
- /*Пусть дана переменная r с радиусом круга. По соответствующей формуле найдите площадь круга и запишите ее в переменную s. Выведите содержимое этой переменной на экран.*/
- let r = 10;
- let s = 3.14*10^2;
- alert ('s')
+//Задание 5 
+let ar = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let str = '';
+for (let i = 0; i < ar.length; i++) {
+    str = str + '-' + ar[i];
+}
+console.log(str);
+
+//Задание 6
+let a = 5;
+let b = 6;
+if (a > 2, a < 11, b >=6, b < 14) {
+    console.log('True')
+}
+else { 
+    console.log('False')
+}
 
