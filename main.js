d199ca623a1558183c8b059394f0dff94a53434e
DanilNefedov <ndanil2@ukr.net>
2021-06-03 23:01:47 +0300

    hw-03.06

--- main.js
+++ main.js
@@ -1,80 +1,103 @@
 
+//even numbers
 
-// // +-календарь
 
-let month = prompt('Введите число от 1 до 12', 0);
-if(month >=1 && month <= 3){
-alert("Winter");
-}else if(month >=4 && month <= 6){
-alert("Spring");
-}else if(month >=7 && month <= 9){
-	alert("Summmer");
-}else if(month >=10 && month <= 12){
-	alert("Autumn");
+ let j = +prompt('Enter number for 0 to 100', 0);
+ let k = 0;
+ for(let i = 0; i <= j ; i++){
+ 	if(i%2==0){
+ 		console.log(i)
+ 		k++
+ 	}
+ }
+console.log(k);
+
+
+
+
+//array sum
+
+
+ let array = [1, 2, 3, 4, 5];
+ let result = 0;
+ for( let i = 0; i < array.length; i++){
+ 	result += array[i];
+ 	}
+ console.log(result);
+
+
+//>3 but <10
+
+
+ let array1 = [2, 5, 9, 15, 0, 4]
+ for(let i = 0; i < array1.length; i++){
+ 	if(array1[i]>3 && array1[i]<10){
+ 		console.log(array1[i])
+ 	}
+ }
+
+
+
+
+
+//sum of positiv elements
+
+
+
+ let array2 = [-1, -2, 0, 2, 1, 3];
+ let sum = 0;
+
+ for(let i = 0; i < array2.length; i++){
+if(array2[i]>=0){
+ 		sum += array2[i]
+ 	}
+ }
+ console.log(sum)
+
+
+
+
+//put '-' 
+
+
+let array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let str='';
+
+for(let i = 0; i < array3.length; i++){
+ 	str=array3[i] + '-';
+console.log(str);
+}
+ 
+
+
+
+
+//true/false
+
+
+
+let a = +prompt('enter first number', 0);
+let b = +prompt('enter second number', 0);
+
+if((a>2 && a<11) || (b>=6 && b<=14)){
+console.log('True')
 }else{
- 	alert("Вы ввели неверное число");
+ 	console.log('False')
 }
 
 
 
- //верно\невенорно
-
-
-
-let test = prompt('Нажмите "ОК"', );
-if( test === true){
-	alert('Верно')
-}else{
- 	alert('Неверно')
-}
-
-
-
-//узнать на счет кнопки "отмена"
-//Секунд в сутках
-
-
-
-let day = +prompt('Введите желаемое число суток', 0);
-
-
-if(day >= 0){
-	let hours = day * 24;
-	let minutes = hours * 60;
-	let seconds = minutes * 60;
-alert('В ваших ' + day + ' сутки ' + seconds + ' секунд')
-}else{
-alert('Введите число')
-}
-
-
-
-
-// площадь круга
-//узнать как исключать типы данных
-
-
-
-let radius = +prompt('Введите радиус круга', 0)
-if(radius >= 0){
- 	let area = Math.PI * Math.pow( radius, 2);
- 	alert(`Площадь вашего круга = ${area}`)
-}else{
-	alert('Введите корректное число')
-}
 
 
 
 
 
 
-//***
-//узнать про десятичные числа 
 
-let num = prompt('Введите число', 0)
-num %= 2;
-if(num === 0){
-	alert('Вы ввели ЧЕТНОЕ число')
-}else{
-	alert('Вы ввели НЕЧЕТНОЕ число')
-}
+
+
+
+
+
+
+
