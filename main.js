6d157a97b7690fe017418a86bca655fe00064aeb
DanilNefedov <ndanil2@ukr.net>
2021-07-05 00:20:25 +0300

    HW string methods

--- main.js
+++ main.js
@@ -1,98 +1,131 @@
-// Дан массив [1, 2, 3]. Добавьте ему в конец элементы 4, 5, 6.
-
-
-let arr = [1,2,3];
-arr.push(4,5,6);
-console.log(arr);
+// 1) пользователь может заливать файлы с расширениями только jpg png. 
+// Если он залил правильный файл - вывести сообщение об успехе, 
+// если нет - об ошибке
 
 
 
-// Дан массив [1, 2, 3]. Добавьте ему в начало элементы 4, 5, 6.
-
-
-let arr1 = [1,2,3];
-arr1.unshift(4, 5, 6);
-console.log(arr1);
-
-
-
-// Дан массив ['js', 'css', 'jq']. Выведите на экран первый элемент. 
-
-
-let arr2 = ['js', 'css', 'jq'];
-let res = arr2.shift();
-console.log(res);
-
-
-
-// Дан массив ['js', 'css', 'jq']. Выведите на экран последний элемент.
-
-
-let arr3 = ['js', 'css', 'jq'];
-let res1 = arr3.pop();
-console.log(res1);
-
-
-
-// Дан массив [1, 2, 3, 4, 5]. 
-//С помощью метода slice запишите в новый элементы [1, 2, 3]. 
-
-
-let arr4 = [1,2,3,4,5];
-console.log(arr4.slice(0, 3));
-
-
-
-// Дан массив [1, 2, 3, 4, 5]. 
-// С помощью метода slice запишите в новый элементы [4, 5].
-
-
-let arr5 = [1,2,3,4,5];
-console.log(arr5.slice(3));
-
-
-
-// Дан массив [1, 2, 3, 4, 5]. 
-// С помощью метода splice преобразуйте массив в [1, 4, 5].
-
-
-let arr6 = [1,2,3,4,5];
-arr6.splice(1,2);
-console.log(arr6);
-
-
-
-//Используйте метод reduce в комбинации с concat для свёртки массива 
-//массивов в один массив, у которого есть все элементы входных массивов.
-//var arrays = [[1, 2, 3], [4, 5], [6]]; 
-// Ваш код
-// → [1, 2, 3, 4, 5, 6]
-
-
-let arrays = [[1, 2, 3], [4, 5], [6]];
-let resArr = arrays.reduce((acc, val) => acc.concat(val), [])
-console.log(resArr)
-    
-
-
-// Дан массив [3, 4, 1, 2, 7]. Отсортируйте его не используя метод sort. 
-// Предпочтительный метод сортировки - пузырьком.
-
-
-
-let bub = [3, 4, 1, 2, 7]
-
-function bubble(arr){
-    for(let j = 0; j < arr.length; j++){
-        for(let i = 0; i < arr.length - 1; i++){
-            if(arr[i] > arr[i+1]){
-               let buff = arr[i];
-               arr[i] = arr[i+1];
-               arr[i+1] = buff; 
-            }
-        }
+function checkPhoto (){
+  let photo = prompt('Отправьте фотографию в jpg/png', 'png')
+   if(photo.includes('png')){
+    console.log('Успех')
+  }else if(photo.includes('jpg')){
+    console.log('Успех')
+  }else{
+    console.log('Ошибка')
     }
-    console.log(arr)
 }
 
-bubble(bub)
+checkPhoto()
+
+
+
+// 2) страница может быть размещена на хостинге и начинаться с http или https. 
+// вне зависимости от этого ссылка в меню должна получать класс  active
+
+
+
+
+
+// 3) функция принимает параметром две строки, сравнить сколько букв 
+// в этих словах повторяются и в каком количестве. вывести как обьект
+
+
+//regex?
+
+// function letters(first, second){
+//   let arr1 = first.split('');
+//   let arr2 = second.split('');
+//   let arrRes1 = [];
+//   let arrRes2 = [];
+//   let obj ={};
+
+//     for(let i in arr1){
+//         if(arrRes1[arr1[i]] != undefined){
+//           obj.firstWordLetter = (arrRes1[arr1[i]] ++)
+//         }else{
+//           (arrRes1[arr1[i]] = 1)
+//         }
+//       } 
+//   console.log(obj)
+
+
+  
+// }
+
+// letters('hello', 'aabbccca')
+
+
+
+
+
+// 4)Дана строка из 3-х цифр. Найдите сумму этих цифр. 
+// То есть сложите как числа первый 
+// символ строки, второй и третий.
+ 
+
+
+let str = '123'
+let sum=0;
+for(let i = 0; i < str.length; i++){
+    let num = str.charAt(i)
+    sum += +num 
+}
+console.log(sum)
+
+
+
+
+
+// 5)Дана строка из 6-ти цифр. Проверьте, что сумма первых трех цифр 
+// равняется сумме вторых трех цифр. Если это так - выведите 'да', 
+// в противном случае выведите 
+// 'нет'.
+
+
+
+let str2 = '654456'
+let sum2 = str2.slice(0, 3);
+let sum3 = str2.slice(3, 6);
+let res1 = 0;
+let res2 = 0;
+
+
+
+for(let i = 0; i < sum2.length; i++){
+    let item = sum2[i];
+    res1 += +item
+}
+
+
+for(let i = 0; i < sum3.length; i++){
+    let item2 = sum3[i];
+    res2 += +item2
+}
+
+if(res1 === res2){
+    console.log('Yes')
+}else{
+    console.log('No')
+}
+
+
+
+
+
+
+
+// 6) в массиве есть обьекты с товарами, у них может быть свойство "акция". 
+// вывести в консоль только те товары, у которых она есть
+
+
+let arr = [
+    {name:'shoes', price:'sale'},
+    {name:'clothes', price:'sale'},
+    {name:'access:', price:'noSale'}
+];
+arr.sort(function (a) {
+    if (a.price == 'sale') {
+        console.log(a.name)
+    }
+});
+//не затрагивается 1-ый объект
