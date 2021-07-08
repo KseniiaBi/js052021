21b0b49f58369d8217eeb8ed071aec4024f3259f
DanilNefedov <ndanil2@ukr.net>
2021-07-08 23:31:55 +0300

    methods arr/ obj

--- main.js
+++ main.js
@@ -1,131 +1,156 @@
-// 1) пользователь может заливать файлы с расширениями только jpg png. 
-// Если он залил правильный файл - вывести сообщение об успехе, 
-// если нет - об ошибке
+// 1) Напишите функцию, которая проверяет, 
+// является ли элемент именно простым объектом, а не массивом, null и т.п.
 
 
-
-function checkPhoto (){
-  let photo = prompt('Отправьте фотографию в jpg/png', 'png')
-   if(photo.includes('png')){
-    console.log('Успех')
-  }else if(photo.includes('jpg')){
-    console.log('Успех')
-  }else{
-    console.log('Ошибка')
-    }
+let x = {
+    user:'2',
+    name:'3',
+    paswowrd:'2'
 }
 
-checkPhoto()
+// function itemCheck(a){
+//     if(typeof a === 'object' && a !== null){
+//         console.log('Object')
+//     }else{
+//         console.log('Not an Object')
+//     }
 
-
-
-// 2) страница может быть размещена на хостинге и начинаться с http или https. 
-// вне зависимости от этого ссылка в меню должна получать класс  active
-
-
-
-
-
-// 3) функция принимает параметром две строки, сравнить сколько букв 
-// в этих словах повторяются и в каком количестве. вывести как обьект
-
-
-//regex?
-
-// function letters(first, second){
-//   let arr1 = first.split('');
-//   let arr2 = second.split('');
-//   let arrRes1 = [];
-//   let arrRes2 = [];
-//   let obj ={};
-
-//     for(let i in arr1){
-//         if(arrRes1[arr1[i]] != undefined){
-//           obj.firstWordLetter = (arrRes1[arr1[i]] ++)
-//         }else{
-//           (arrRes1[arr1[i]] = 1)
-//         }
-//       } 
-//   console.log(obj)
-
-
-  
 // }
 
-// letters('hello', 'aabbccca')
+let res = (a) => (typeof a === 'object' && a !== null) ? 'Obj' : 'Not an Obj';
+
+console.log(res(x))
 
 
 
+//2)Напишите функцию, которая возвращает новый объект без указанных значений.
+//* Ожидаемый результат: ({ a: 1, b: 2 }, 'b') => { a: 1 }
 
 
-// 4)Дана строка из 3-х цифр. Найдите сумму этих цифр. 
-// То есть сложите как числа первый 
-// символ строки, второй и третий.
- 
 
-
-let str = '123'
-let sum=0;
-for(let i = 0; i < str.length; i++){
-    let num = str.charAt(i)
-    sum += +num 
+function findDel(a, b){
+    delete a[b];
+    return a
 }
-console.log(sum)
+console.log(findDel({a:1, b:2}, 'b'))
 
 
 
+//3)Описание задачи: Напишите функцию, которая делает поверхностную проверку 
+// объекта на пустоту.
+//   * Ожидаемый результат: ({}) => true,
+//       ({ a: undefined }) => true,
+//       ({ a: 1 }) => false
+//   * Пустые значения: '', null, NaN, undefined
 
 
-// 5)Дана строка из 6-ти цифр. Проверьте, что сумма первых трех цифр 
-// равняется сумме вторых трех цифр. Если это так - выведите 'да', 
-// в противном случае выведите 
-// 'нет'.
-
-
-
-let str2 = '654456'
-let sum2 = str2.slice(0, 3);
-let sum3 = str2.slice(3, 6);
-let res1 = 0;
-let res2 = 0;
-
-
-
-for(let i = 0; i < sum2.length; i++){
-    let item = sum2[i];
-    res1 += +item
+function findEmpt(obj){
+    for(let key in obj){
+        if(obj[key] === '' || null){
+            return 'true'
+        }else if(obj[key] === NaN || undefined){
+            return 'true'
+        }else{
+            return 'false'
+        }
+        return obj
+    }   
 }
+   
+
+console.log(findEmpt({a:NaN}))
+
+//dont work
 
 
-for(let i = 0; i < sum3.length; i++){
-    let item2 = sum3[i];
-    res2 += +item2
+
+//4)Напишите функцию, которая поверхностно сравнивает два объекта.
+//* Ожидаемый результат: True если объекты идентичны, 
+//false если объекты разные ({ a: 1, b: 1 }, { a: 1, b: 1 }) => true
+
+
+
+let obj2 = {
+    a: '1',
+    b: '2'
 }
-
-if(res1 === res2){
-    console.log('Yes')
-}else{
-    console.log('No')
+let obj3 = {
+    a: '1',
+    b: '2'
 }
-
-
-
-
-
-
-
-// 6) в массиве есть обьекты с товарами, у них может быть свойство "акция". 
-// вывести в консоль только те товары, у которых она есть
-
-
-let arr = [
-    {name:'shoes', price:'sale'},
-    {name:'clothes', price:'sale'},
-    {name:'access:', price:'noSale'}
-];
-arr.sort(function (a) {
-    if (a.price == 'sale') {
-        console.log(a.name)
+function comparison(a, b) {
+    for(let key in a) {
+        if(!(key in b) || a[key] !== b[key]) {
+            return false;
+        }
     }
-});
-//не затрагивается 1-ый объект
+    for(let key in b) {
+        if(!(key in a) || a[key] !== b[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(comparison(obj2, obj3))
+
+
+
+
+
+
+// 5 Напишите функцию, которая вызывает метод массива на 
+//заданный путь объекта.
+// /**
+//   * Описание задачи: Напишите функцию, которая вызывает 
+//метод массива на заданный путь объекта.
+//   * Ожидаемый результат: ({ a: { b: [1, 2, 3] } }, 'a.b', splice, [1, 2]) => [2, 3]
+// Пример использования: const data = { a: { b: [1, 2, 3] } }
+// console.log(invoke(data, 'a.b', 'splice', [1, 2])); // [2, 3]
+
+
+
+// Напишите функцию, которая очищает массив от нежелательных значений, 
+// таких как false, undefined, пустые строки, ноль, null.
+
+
+
+
+let filtered = [12, 5, 0, 130, 44, undefined, NaN, 1, '', , false, 2]
+
+let func = filtered.filter(function(c){
+    if(c !== false || undefined){
+        return c
+    }else if(c !== '' || 0){
+        return c
+    }else if(c !== null){
+        return c
+    }else{
+        true
+    }
+})
+
+console.log(func);
+
+
+
+
+
+// 7) Напишите функцию, которая убирает повторяющиеся значения.
+//   * Ожидаемый результат: [1, 2, 3, 1, 2] => [1, 2, 3]
+
+
+let arr5 = [1,2,3,1,2]
+
+let deleteElement = arr5.filter(function(item, pos) {
+    return arr5.indexOf(item) == pos;
+})
+
+console.log(deleteElement)
+
+
+
+//8)Напишите функцию, которая разделяет массив на части заданного размера.
+//* Ожидаемый результат: ([1, 2, 3, 4, 5], 2) => [[1, 2], [3, 4], [5]]
+
+
+
