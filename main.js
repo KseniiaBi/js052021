522e0490f0f34a47470f9692ada257630320389b
DanilNefedov <ndanil2@ukr.net>
2021-07-22 18:38:40 +0300

    dont work

--- main.js
+++ main.js
@@ -17,29 +17,91 @@ const four = document.getElementById('four');
 const seven = document.getElementById('seven');
 const input = document.getElementById('input');
 const symbol = document.getElementsByClassName('symbol');
-// const btn = document.querySelector('button');
+const btn = document.querySelector('button');
 // const span = document.querySelector('span');
 
 let arrNumb = [];
 
+//убрать обновление страницы при нажатии на кнопку
 
-//как обратиться ко всем span or className
 
-// seven.onclick=()=>{
-//     console.log(input.value);
-// }
 
-function insert(num){
-    input.value = input.value + num;
+
+
+one.onclick = clickOne;
+function clickOne(){
+    input.value += one.innerHTML;
 }
 
-function clearn(){
-    input.value = '';
+
+two.onclick = clickTwo;
+function clickTwo(){
+    input.value += two.innerHTML;
 }
 
-function equal(){
-    let res = input.value;
-    if(res){
-        input.value = eval(res);
-    }
+
+three.onclick = clickThree;
+function clickThree(){
+    input.value += three.innerHTML;
 }
+
+
+four.onclick = clickFour;
+function clickFour(){
+    input.value += four.innerHTML;
+}
+
+
+five.onclick = clickFive;
+function clickFive(){
+    input.value += five.innerHTML;
+}
+
+
+six.onclick = clicSix;
+function clicSix(){
+    input.value += six.innerHTML;
+}
+
+
+seven.onclick = clickSeven;
+function clickSeven(){
+    input.value += seven.innerHTML;
+}
+
+
+eight.onclick = clickEight;
+function clickEight(){
+    input.value += eight.innerHTML;
+}
+
+
+nine.onclick = clickNine;
+function clickNine(){
+    input.value += nine.innerHTML;
+}
+
+
+zero.onclick = clickZero;
+function clickZero(){
+    input.value += zero.innerHTML;
+}
+
+
+c.onclick = () => {
+    input.value = "";
+};
+
+
+point.onclick = clickPoint;
+function clickPoint() {
+    input.value += point.innerHTML;
+}
+
+
+plus.onclick=()=>{
+    input.value = arrNumb.push(input.value);
+    
+}
+
+
