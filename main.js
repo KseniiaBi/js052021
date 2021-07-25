025c9d23f14bf6a3f45bb7200df4f729c5c9b858
DanilNefedov <ndanil2@ukr.net>
2021-07-25 22:59:59 +0300

    tabs/ and two pages link to tabs

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const tabs = document.querySelectorAll('.tab-btn');
-const tabContent = document.querySelectorAll('.tab')
+const tabContent = document.querySelectorAll('.tab');
+
+
+
+const tabSecond = document.getElementById('tab-2');
+const tabBtnSec = document.querySelector('.tab-btn2');
+
+
+const tabThird = document.getElementById('tab-3');
+const tabBtnThird = document.querySelector('.tab-btn3')
+
+const links = document.querySelectorAll('.link');
+
+
+for(let link of links){
+    link.onclick = function(){
+        
+        let tab2 = document.getElementById('tab-2');
+    }
+}
+
 
 
 
@@ -9,8 +29,10 @@ const tabContent = document.querySelectorAll('.tab')
 for(let tab of tabs){//поиск каждого элемента кнопок
     tab.onclick = function(){//если нажимаем на кнопку выполняем функцию
 
-        let tabID = tab.getAttribute("data-tab");//в новой переменной ищем дата атрибута по названию и запоминаем каждый id data-tab
+        let tabID = tab.getAttribute('data-tab');//в новой переменной ищем дата атрибута по названию и запоминаем каждый id data-tab
         let currentTab = document.querySelector(tabID);//ищем такой же айдишник как и в дата атрибуте
+
+
 
         for(let item of tabs){//проходимся по каждому табу и удаляем у них класс active
             item.classList.remove('active');
@@ -22,5 +44,28 @@ for(let tab of tabs){//поиск каждого элемента кнопок
 
         tab.classList.add('active');//добавляем табу класс
         currentTab.classList.add('active');//добавляем контенту класс
+    }
+    if(location.hash == '#tab-2'){
+        
+        for(let item of tabs){
+            item.classList.remove('active');
+        }
+        for(let content of tabContent){
+            content.classList.remove('active')
+        }
+        tabSecond.classList.add('active')
+        tabBtnSec.classList.add('active')
+    }
+
+    if(location.hash == '#tab-3'){
+        
+        for(let item of tabs){
+            item.classList.remove('active');
+        }
+        for(let content of tabContent){
+            content.classList.remove('active')
+        }
+        tabThird.classList.add('active')
+        tabBtnThird.classList.add('active')
     }
 }
