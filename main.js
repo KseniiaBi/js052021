f64d8e8b73ee227e3582086a8dc8526df660632a
DanilNefedov <ndanil2@ukr.net>
2021-07-25 13:25:17 +0300

    tabs

--- main.js
+++ main.js
@@ -1,47 +1,26 @@
-const equally = document.getElementById('equally');
-const minus = document.getElementById('minus');
-const plus = document.getElementById('plus');
-const multiplic = document.getElementById('multiplic');
-const devision = document.getElementById('devision');
-const point = document.getElementById('point');
-const three = document.getElementById('three');
-const six = document.getElementById('six');
-const nine = document.getElementById('nine');
-const zero = document.getElementById('zero');
-const two = document.getElementById('two');
-const five = document.getElementById('five');
-const eight = document.getElementById('eight');
-const c = document.getElementById('c');
-const one = document.getElementById('one');
-const four = document.getElementById('four');
-const seven = document.getElementById('seven');
-const input = document.getElementById('input');
-const symbol = document.getElementsByClassName('symbol');
-const btn = document.querySelector('button');
-// const span = document.querySelector('span');
-
-let arrNumb = [];
-
-//убрать обновление страницы при нажатии на кнопку
+const tabs = document.querySelectorAll('.tab-btn');
+const tabContent = document.querySelectorAll('.tab')
 
 
 
 
 
-function insert(num){
-    input.value = input.value + num;
-}
 
-function clearn(){
-    input.value = '';
-}
+for(let tab of tabs){//поиск каждого элемента кнопок
+    tab.onclick = function(){//если нажимаем на кнопку выполняем функцию
 
-function equal(){
-    let res = input.value;
-    if(res){
-        input.value = eval(res);
+        let tabID = tab.getAttribute("data-tab");//в новой переменной ищем дата атрибута по названию и запоминаем каждый id data-tab
+        let currentTab = document.querySelector(tabID);//ищем такой же айдишник как и в дата атрибуте
+
+        for(let item of tabs){//проходимся по каждому табу и удаляем у них класс active
+            item.classList.remove('active');
+        }
+
+        for(let content of tabContent){//проходимся по каждому элементу и удаляем класс active
+            content.classList.remove('active')
+        }
+
+        tab.classList.add('active');//добавляем табу класс
+        currentTab.classList.add('active');//добавляем контенту класс
     }
 }
-
-
-    
