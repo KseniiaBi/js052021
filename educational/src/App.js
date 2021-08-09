630e6110bacf84386d108bc9752e313863e1f1f4
Kseniia Burkun <kseniia.bidenko@gmail.com>
2021-08-09 20:40:45 +0300

    Clock with statefull component

--- educational/src/App.js
+++ educational/src/App.js
@@ -1,47 +1,29 @@
 import './App.css';
+import React from 'react';
 
-let cakes = [
-  {
-    img: 'https://cdn.igp.com/f_auto,q_auto,t_prodm/products/p-delicious-chocolate-cake-with-premium-frosting-2-kg--135598-m.jpg',
-    name: 'Cheesecake',
-    price: 19.90
-  },
-  {
-    img: 'https://www.hummingbirdhigh.com/wp-content/uploads/2020/05/01_Rainbow-Unicorn-Cake_IMG_0008.jpg',
-    name: 'Bisquit',
-    price: 9.90
-  },
-  {
-    img: 'https://i0.wp.com/bestcakedesign.com/wp-content/uploads/chocolate-cake-design-11.png',
-    name: 'Red velvet',
-    price: 29.90
-  },
-  {
-    img: 'https://cafedelites.com/wp-content/uploads/2020/02/Chocolate-Cake-IMAGE-99.jpg',
-    name: 'Prague',
-    price: 14.90
+// statefull Component
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+      this.state = {
+        time: new Date()
+      };
   }
-];
 
-function App() {
-  return (
-    <div className="App">
-     {
-      cakes.map((cake, index) => <Cake key={index} img={cake.img} name={cake.name} price={cake.price} />)
-     }
-    </div>
-  );
+  tick(){
+    this.setState({time: new Date()});
+  }
+
+  componentDidMount(){
+    setInterval(() => this.tick(), 1000);
+  }
+
+
+  render(){
+    return (
+      <h1>Time is: {this.state.time.toLocaleTimeString()}</h1>
+      )
+  }
 }
 
-function Cake(props){
-  return(
-      <div className="cake">
-        <img src={props.img} alt={props.name} />
-        <h4>{props.name}</h4>
-        <h2>${props.price}</h2>
-      </div>
-    );
-}
-
-
-export default App;
+export default Clock;
