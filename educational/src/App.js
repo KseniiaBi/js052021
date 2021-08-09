ac037440ecf2787f662e54ae0900ee1022b31a1b
Kseniia Burkun <kseniia.bidenko@gmail.com>
2021-08-09 20:03:20 +0300

    cakes react shop

--- educational/src/App.js
+++ educational/src/App.js
@@ -1,21 +1,47 @@
 import './App.css';
 
-let name = 'User';
+let cakes = [
+  {
+    img: 'https://cdn.igp.com/f_auto,q_auto,t_prodm/products/p-delicious-chocolate-cake-with-premium-frosting-2-kg--135598-m.jpg',
+    name: 'Cheesecake',
+    price: 19.90
+  },
+  {
+    img: 'https://www.hummingbirdhigh.com/wp-content/uploads/2020/05/01_Rainbow-Unicorn-Cake_IMG_0008.jpg',
+    name: 'Bisquit',
+    price: 9.90
+  },
+  {
+    img: 'https://i0.wp.com/bestcakedesign.com/wp-content/uploads/chocolate-cake-design-11.png',
+    name: 'Red velvet',
+    price: 29.90
+  },
+  {
+    img: 'https://cafedelites.com/wp-content/uploads/2020/02/Chocolate-Cake-IMAGE-99.jpg',
+    name: 'Prague',
+    price: 14.90
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, {name}!</h1>
-      <Button text="Click me!" handler={sayHi} />
+     {
+      cakes.map((cake, index) => <Cake key={index} img={cake.img} name={cake.name} price={cake.price} />)
+     }
     </div>
   );
 }
 
-function sayHi(){
-  alert('Hi!');
+function Cake(props){
+  return(
+      <div className="cake">
+        <img src={props.img} alt={props.name} />
+        <h4>{props.name}</h4>
+        <h2>${props.price}</h2>
+      </div>
+    );
 }
 
-// простейший компонент
-let Button = (props) => <button onClick={props.handler}>{props.text}</button>;
 
 export default App;
